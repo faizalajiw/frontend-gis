2f41facefcf8c46b5d4a26338973c28dadebd0cf
faizalajiw <faizalajiwibowo7@gmail.com>
2023-01-19 09:38:29 +0700

    setup dan installasi library

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+//react router dom
+import { BrowserRouter } from 'react-router-dom';
+//boostrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+//mapbox gl css
+import 'mapbox-gl/dist/mapbox-gl.css';
+//mapbox gl geocoder css
+import 'mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+//mapbox gl directions css
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
+//import custom css
+import './assets/css/styles.css';
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
