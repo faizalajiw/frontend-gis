929a581eec44e6c0939d59844549b469597a759d
faizalajiw <faizalajiwibowo7@gmail.com>
2023-01-20 03:59:17 +0700

    konfigurasi private route categories

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import { Switch, Route } from "react-router-dom";
 import Login from "../pages/admin/Login";
 import PrivateRoute from "./PrivateRoutes";
 import Dashboard from "../pages/admin/dashboard/Index";
+import CategoriesIndex from "../pages/admin/categories/Index";
 
 function Routes() {
   return (
@@ -17,6 +18,12 @@ function Routes() {
         <Dashboard />
       </PrivateRoute>
       {/* end private route "/admin/dashboard" */}
+
+      {/* private route "/admin/categories" */}
+      <PrivateRoute exact path="/admin/categories">
+        <CategoriesIndex />
+      </PrivateRoute>
+      {/* end private route "/admin/categories" */}
     </Switch>
   );
 }
