bc80864ac5e99a090756497e6f961782a6c3bf34
faizalajiw <faizalajiwibowo7@gmail.com>
2023-01-20 03:57:34 +0700

    membuat component pagination

---
+++ src/components/utilities/Pagination.jsx
@@ -0,0 +1,21 @@
+//import pagination
+import Pagination from "react-js-pagination";
+
+function PaginationComponent(props) {
+  return (
+    props.total > 0 && (
+      <Pagination
+        innerClass={`pagination justify-content-${props.position} mb-0`} //posisi halaman
+        activePage={props.currentPage} //halaman yg sedang aktif
+        activeClass="page-item active" 
+        itemsCountPerPage={props.perPage} //jumlah data per halaman
+        totalItemsCount={props.total} //total data 
+        onChange={props.onChange} //mengganti halaman
+        itemClasss="page-item"
+        linkClass="page-link"
+      />
+    )
+  );
+}
+
+export default PaginationComponent;
