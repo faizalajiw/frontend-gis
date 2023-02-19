6cc5b058219f9a94797e4fee07e28b8f93f14df7
faizalajiw <faizalajiwibowo7@gmail.com>
2023-02-19 12:11:25 +0700

    membuat tampilan home

--- src/components/utilities/CardCategory.jsx
+++ src/components/utilities/CardCategory.jsx
@@ -1,10 +1,9 @@
-//import react router dom
 import { Link } from "react-router-dom";
 
 function CardCategory(props) {
 
     return (
-        <div className="col-6 col-md-3 mb-4" key={props.id}>
+        <div className="col-6 col-md-3 mb-5" key={props.id}>
             <Link to={`/category/${props.slug}`} className="text-decoration-none text-dark">
                 <div className="card h-100 border-0 rounded shadow-sm">
                     <div className="card-body text-center">
