5280a039cea306103efce350c1dc0d426ba785a4
faizalajiw <faizalajiwibowo7@gmail.com>
2023-02-19 16:01:17 +0700

    styling halaman tempat

--- src/components/utilities/CardPlace.jsx
+++ src/components/utilities/CardPlace.jsx
@@ -5,20 +5,21 @@ function CardPlace(props) {
     return (
         <div className="col-md-6 mb-5" key={props.id}>
             <Link to={`/places/${props.slug}`} className="text-decoration-none text-dark">
-                <div className="card border-0 rounded shadow-sm mb-3" style={{ maxWidth: "540px" }}>
+                <div className="card border-0 rounded shadow-sm mb-3" style={{ maxWidth: "640px" }}>
                     <div className="row g-0">
                         <div className="col-md-4">
                             {
                                 props.images.slice(0, 1).map((placeImage) => (
-                                    <img src={placeImage.image} className="img-fluid rounded-start" alt="..." style={{ height: "100%", objectFit: "cover" }} key={placeImage.id} />
+                                    <img src={placeImage.image} className="img-fluid rounded-start" alt="..." style={{ width: "100%", height: "100%", objectFit: "cover" }} key={placeImage.id} />
                                 ))
                             }
                         </div>
                         <div className="col-md-8">
                             <div className="card-body">
-                                <h5 className="card-title">{props.title}</h5>
                                 <br />
+                                <h5 className="card-title fw-semibold">{props.title}</h5>
                                 <p className="card-text"><i className="fas fa-map-marker-alt me-2"></i>{props.address}</p>
+                                <br />
                             </div>
                         </div>
                     </div>
---
+++ src/pages/web/places/IndexPlace.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import LayoutWeb from "../../../layouts/Web";
+//import BASE URL API
+import Api from "../../../api";
+
+import CardPlace from "../../../components/utilities/CardPlace";
+import PaginationComponent from "../../../components/utilities/Pagination";
+
+function WebPlaceIndex() {
+    //title page
+    document.title = "Travelin - Tempat";
+
+    //state 
+    const [places, setPlaces] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [perPage, setPerPage] = useState(0);
+    const [total, setTotal] = useState(0);
+
+    //function "fetchDataPlaces"
+    const fetchDataPlaces = async (pageNumber) => {
+        //define variable "page"
+        const page = pageNumber ? pageNumber : currentPage;
+
+        //fetching Rest API
+        await Api.get(`/api/web/places?page=${page}`)
+            .then((response) => {
+                //set data to state "places"
+                setPlaces(response.data.data.data);
+                //set currentPage
+                setCurrentPage(response.data.data.current_page);
+                //set perPage
+                setPerPage(response.data.data.per_page);
+                //total
+                setTotal(response.data.data.total);
+            })
+    }
+
+    //hook
+    useEffect(() => {
+        //call function "fetchDataPlaces"
+        fetchDataPlaces();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+        <React.Fragment>
+            <LayoutWeb>
+                <div className="container mt-100">
+                    <div className="row">
+                        <div className="col-md-12 text-center">
+                            <h4 className="fw-semibold">Tempat</h4>
+                            <br />
+                        </div>
+                        {
+                            places.length > 0
+                                ? places.map((place) => (
+                                    <CardPlace
+                                        key={place.id}
+                                        id={place.id}
+                                        slug={place.slug}
+                                        title={place.title}
+                                        images={place.images}
+                                        address={place.address}
+                                    />
+                                ))
+                                : <div className="alert alert-danger border-0 rounded shadow-sm" role="alert">
+                                    <strong>Data Belum Tersedia!</strong>
+                                </div>
+                        }
+                    </div>
+                    <PaginationComponent
+                        currentPage={currentPage}
+                        perPage={perPage}
+                        total={total}
+                        onChange={(pageNumber) => fetchDataPlaces(pageNumber)}
+                        position="center"
+                    />
+                </div>
+            </LayoutWeb>
+        </React.Fragment>
+    )
+
+}
+
+export default WebPlaceIndex;
