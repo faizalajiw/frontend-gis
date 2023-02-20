82d6a1d05eac348891350b0dac38e5fec6ba7509
faizalajiw <faizalajiwibowo7@gmail.com>
2023-02-20 11:23:40 +0700

    styling footer

--- src/components/web/Footer.jsx
+++ src/components/web/Footer.jsx
@@ -7,7 +7,7 @@ function WebFooter() {
             <div className="footer-custom mt-100">
                 <div className="fs-6">
                     <div className="py-3 text-center text-text-sm-start">
-                        Copyright © 2023 | Travelin by Faizal Aji Wibowo
+                        © 2023 Travelin by ❤ Faizal Aji Wibowo
                     </div>
                 </div>
             </div>
