5218bee998052ee869076c418f484dc7f519a0c1
faizalajiw <faizalajiwibowo7@gmail.com>
2023-02-20 11:23:20 +0700

    styling card kategori

--- src/components/utilities/CardCategory.jsx
+++ src/components/utilities/CardCategory.jsx
@@ -5,11 +5,12 @@ function CardCategory(props) {
     return (
         <div className="col-6 col-md-3 mb-5" key={props.id}>
             <Link to={`/category/${props.slug}`} className="text-decoration-none text-dark">
-                <div className="card h-100 border-0 rounded shadow-sm">
+                <div className="card h-100 border-0 rounded-3 shadow-sm">
                     <div className="card-body text-center">
                         <img src={props.image} style={{ width: "80px" }} alt=""/>
-                        <hr/>
-                        <h6>{props.name.toUpperCase()}</h6>
+                        <br/>
+                        <br/>
+                        <h5>{props.name.toUpperCase()}</h5>
                     </div>
                 </div>
             </Link>
